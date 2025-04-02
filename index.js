3af1b226407c9cfaecfaba4c37fe518bd5100979
Nishant Dakchand Punde <pundenishant@gmail.com>
2025-04-02 23:53:42 +0530

    fixed

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ io.on('connection', (socket) => {
 
 app.use(cors());
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.use('/v1/superadmin', superadmin);
